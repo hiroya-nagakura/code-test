372998d53163b4e6d934f7c154fb1cbcb88433f1
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-02-03 11:41:08 +0900

    一応完成

--- code_test.rb
+++ code_test.rb
@@ -4,12 +4,22 @@ distance = []
 File.foreach("taxi.log"){|line|
   line = line.split(' ') # 半角で区切る
   time.push(line[0].split(':').map{|n| n.to_i}) #記録時間を時：分：秒で区切って格納
-  distance.push(line[1].to_f/1000) # 距離（km）を格納
+  distance.push(line[1].to_f) # 距離（km）を格納
 }
 #計算
 num = 1
+sum =0;
+ans = 0;
 while num < distance.length do
   ridetime = (time[num][0] - time[num-1][0])*3600 + (time[num][1] - time[num-1][1])*60 +(time[num][2] - time[num-1][2]) #乗車時間を秒で算出
-  p distance[num]/(ridetime.to_f/3600)
+  sum += distance[num]
+  if (distance[num]/1000)/(ridetime.to_f/3600) < 10
+    ans = ridetime/85 * 80
+  end 
   num += 1
+end
+if sum < 1052
+  p "420円"
+else 
+  p "(#{ans+1052+((sum-1052)/233+1)*80}円"
 end
