3dd5baef3fe33181c81abee2b2bacc6d6f924860
hiroya-nagakura <nagahiroda@hotmail.co.jp>
2021-02-03 11:44:59 +0900

    修正

--- code_test.rb
+++ code_test.rb
@@ -12,8 +12,8 @@ sum =0;
 ans = 0;
 while num < distance.length do
   ridetime = (time[num][0] - time[num-1][0])*3600 + (time[num][1] - time[num-1][1])*60 +(time[num][2] - time[num-1][2]) #乗車時間を秒で算出
-  sum += distance[num]
-  if (distance[num]/1000)/(ridetime.to_f/3600) < 10
+  sum += distance[num] #距離を月山
+  if (distance[num]/1000)/(ridetime.to_f/3600) <= 10 #時速10k以下の時の処理
     ans = ridetime/85 * 80
   end 
   num += 1
@@ -21,5 +21,5 @@ end
 if sum < 1052
   p "420円"
 else 
-  p "(#{ans+1052+((sum-1052)/233+1)*80}円"
+  p "#{ans+1052+((sum-1052)/233+1).to_i*80}円" #1052mになった時点で+80円になるよう計算
 end
